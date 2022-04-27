b69c12ae020ea7929b2eb582d9a6e71a83a9bda7
Caio Luiz Cardoso Oliveira <ccardoso210@gmail.com>
2022-04-26 21:02:36 -0300

    Configurando o front e botão de adicionar/useState

--- cardapio-o-geladao/src/PaletaLista.jsx
+++ cardapio-o-geladao/src/PaletaLista.jsx
@@ -1,16 +1,30 @@
+import React, {useState} from 'react';
 import './PaletaLista.css'
 import {paletas} from "mocks/paletas"
 function PaletaLista() {
+  const [paletaSelecionada, setPaletaSelecionada] = useState({});
+  const adicionarItem = (paletaIndex) => {
+          const paleta = { [paletaIndex]: Number(paletaSelecionada[paletaIndex] || 0) +1 }
+          setPaletaSelecionada({ ...paletaSelecionada, ...paleta});
+  }
+  const removerItem = (paletaIndex) => {
+    const paleta = {[paletaIndex]: Number(paletaSelecionada[paletaIndex] || setPaletaSelecionada()) -1 }
+    setPaletaSelecionada({...paletaSelecionada, ...paleta});
+  }
   return (
     <div className="PaletaLista">
       {paletas.map((paleta, index) =>
-       <div className="PaletaListaItem" key={`PaletaListaItem-${index}`}>
+      <div className="PaletaListaItem" key={`PaletaListaItem-${index}`}>
+        <span className="PaletaListaItem__badge"> {paletaSelecionada[index] || 0} </span>
+        <span className="PaletaListaItem__bagdeRemover" onClick={() => removerItem(index)}>{paletaSelecionada[index] > 0}-</span>
         <div>
             <div className="PaletaListaItem__titulo">{paleta.titulo}</div>
             <div className='PaletaListaItem__preco'>R${paleta.preco.toFixed(2)}</div>
             <div className='PaletaListaItem__descricao'>{paleta.descricao}</div>
             <div className='PaletaListaItem__acoes Acoes'>
-                <button className='Acoes__adicionar Acoes__adicionar--preencher'>Adicionar</button>
+                <button className='Acoes__adicionar Acoes__adicionar--preencher' 
+                onClick={() => adicionarItem(index)}>Adicionar
+                </button>
             </div>
         </div>
         <img
@@ -18,8 +32,8 @@ function PaletaLista() {
           src={paleta.foto}
           alt={`Paleta de ${paleta.sabor}`}
         />
-       </div>
-      )}
+      </div>
+      )};
     </div>
   );
 }
