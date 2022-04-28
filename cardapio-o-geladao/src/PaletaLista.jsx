8dd55e270dd6963178b260baad3dff271b41265e
Caio Luiz Cardoso Oliveira <ccardoso210@gmail.com>
2022-04-28 19:22:35 -0300

    Configuração do badge counter

--- cardapio-o-geladao/src/PaletaLista.jsx
+++ cardapio-o-geladao/src/PaletaLista.jsx
@@ -11,12 +11,15 @@ function PaletaLista() {
     const paleta = {[paletaIndex]: Number(paletaSelecionada[paletaIndex] || setPaletaSelecionada()) -1 }
     setPaletaSelecionada({...paletaSelecionada, ...paleta});
   }
+  const badgeCounter = (canRender, index) =>
+	Boolean(canRender) && (<span className="PaletaListaItem__badge"> {paletaSelecionada[index]} </span>, <span className="PaletaListaItem__bagde" id="badge__remover"onClick={() => removerItem(index)}>{paletaSelecionada[index] > 0}-</span>);
   return (
     <div className="PaletaLista">
       {paletas.map((paleta, index) =>
       <div className="PaletaListaItem" key={`PaletaListaItem-${index}`}>
+        {badgeCounter(paletaSelecionada[index], index)}
         <span className="PaletaListaItem__badge"> {paletaSelecionada[index] || 0} </span>
-        <span className="PaletaListaItem__bagdeRemover" onClick={() => removerItem(index)}>{paletaSelecionada[index] > 0}-</span>
+        <span className="PaletaListaItem__bagde" id="badge__remover"onClick={() => removerItem(index)}>{paletaSelecionada[index] > 0}-</span>
         <div>
             <div className="PaletaListaItem__titulo">{paleta.titulo}</div>
             <div className='PaletaListaItem__preco'>R${paleta.preco.toFixed(2)}</div>
